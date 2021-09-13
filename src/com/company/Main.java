b1c83f05aa9bca05fd0462ef297624e343010e2c
vTim <jofill@mail.ru>
2021-09-13 17:57:35 +0300

    смержил

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello");
+
+
      StringBuilder sb = new StringBuilder("Test");
         System.out.println(sb.toString());
         sb.append(" test");
