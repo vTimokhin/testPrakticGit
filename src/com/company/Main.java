e67a80afd09692f3389b37ef0fb795701a2995f5
vTim <jofill@mail.ru>
2021-09-13 17:09:01 +0300

    Еще добавил вывод

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
      StringBuilder sb = new StringBuilder("Test");
+        System.out.println(sb.toString());
+        sb.append(" test");
+        System.out.println(sb.toString());
+        sb.append(" tESt").append(" TesT");
         System.out.println(sb);
+
     }
 }
