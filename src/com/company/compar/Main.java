79e01119c4a43f4413e2b783288bae3c2a0b005f
vTim <jofill@mail.ru>
2021-10-04 23:42:25 +0300

    отсортировал двумя способами

--- src/com/company/compar/Main.java
+++ src/com/company/compar/Main.java
@@ -3,21 +3,39 @@ package com.company.compar;
 import com.company.Person;
 import com.company.Sex;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-    Set persons = new TreeSet();
-    persons.add(new Person("Tom", 22, Sex.FEMALE));
-    persons.add(new Person("Tim", 12, Sex.MALE));
-    persons.add(new Person("Cristy", 34, Sex.MALE));
-    persons.add(new Person("Lisy", 53, Sex.FEMALE));
-        for (Object p: persons
-             ) {
+        Set<Person> persons = new TreeSet<>();
+        persons.add(new Person("Tom", 22, Sex.FEMALE));
+        persons.add(new Person("Tim", 12, Sex.MALE));
+        persons.add(new Person("Cristy", 34, Sex.MALE));
+        persons.add(new Person("Lisy", 53, Sex.FEMALE));
+        for (Object p : persons
+        ) {
             System.out.println(p);
 
         }
+        System.out.println();
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Tom", 22, Sex.FEMALE));
+        personList.add(new Person("Tim", 12, Sex.MALE));
+        personList.add(new Person("Cristy", 34, Sex.MALE));
+        personList.add(new Person("Lisy", 53, Sex.FEMALE));
+
+        NameComparator nameComparator = new NameComparator();
+
+        personList.sort(nameComparator);
+        for (Person p :
+                personList) {
+            System.out.println(p);
+        }
+
+
     }
 }
 
