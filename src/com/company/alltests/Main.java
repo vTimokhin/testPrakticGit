603e30408fedcfa096d0a074801c0537a850545b
vTim <jofill@mail.ru>
2021-10-07 16:36:10 +0300

    общая ветка для тестов и потоки

---
+++ src/com/company/alltests/Main.java
@@ -0,0 +1,11 @@
+package com.company.alltests;
+
+public class Main {
+    public static final String MERCURY = "Mercury";
+    public static final String MERCURYBOG = MERCURY;
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/company/alltests/Threads.java
@@ -0,0 +1,18 @@
+package com.company.alltests;
+
+public class Threads {
+    public static void main(String[] args) {
+        MyThreads myThread1 = new MyThreads();
+        myThread1.run();
+        System.out.println("Main");
+    }
+}
+
+class MyThreads extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Potok_1 " + i);
+
+        }
+    }
+}
